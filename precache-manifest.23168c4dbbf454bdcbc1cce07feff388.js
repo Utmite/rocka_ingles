18fb46ab290f0d9680f61509eced68afe7c6c989
rocka <rockimunoz94@gmail.com>
2021-12-16 23:47:33 -0300

    'deplay'

---
+++ precache-manifest.23168c4dbbf454bdcbc1cce07feff388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85218d79098e7c0173b",
+    "url": "/rocka_ingles/css/app.4eefdf34.css"
+  },
+  {
+    "revision": "faaafbb0a66e56fb81debb25aa625911",
+    "url": "/rocka_ingles/index.html"
+  },
+  {
+    "revision": "91a8974f79140252486d",
+    "url": "/rocka_ingles/js/about.fcb918d9.js"
+  },
+  {
+    "revision": "f85218d79098e7c0173b",
+    "url": "/rocka_ingles/js/app.e1a12023.js"
+  },
+  {
+    "revision": "65c4fed4f9244cbe8b13",
+    "url": "/rocka_ingles/js/chunk-vendors.8c9ae045.js"
+  },
+  {
+    "revision": "a3b9344d757f627aec9f6609b93ae81b",
+    "url": "/rocka_ingles/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rocka_ingles/robots.txt"
+  }
+]);
